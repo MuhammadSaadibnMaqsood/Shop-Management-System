82245d01f8992bbd735efb396cb0931c943dc7d3
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-08-18 00:18:55 -0700

    Create signup

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 const PORT = process.env.PORT ||3000;
@@ -10,6 +12,13 @@ connectDB();
 
 // app setup
 const app = express();
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// routes
+
+app.use('/api/user',userRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
