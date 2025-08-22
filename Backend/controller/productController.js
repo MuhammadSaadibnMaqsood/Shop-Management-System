0facc2071864dc45039c71018245476574ecaf96
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-08-22 05:48:45 -0700

    Complete the getallproductsand specific owner products controller

--- Backend/controller/productController.js
+++ Backend/controller/productController.js
@@ -2,8 +2,7 @@ import express from "express";
 import shopModel from "../models/shop.js";
 import productModel from "../models/product.js";
 
-
-// FUNCTION TO CREATE PRODUCT 
+// FUNCTION TO CREATE PRODUCT
 export async function createProduct(req, res) {
   try {
     const { productName, price, description, category, stock, brand } =
@@ -21,11 +20,9 @@ export async function createProduct(req, res) {
     }
 
     if (price <= 0 || stock < 0) {
-      return res
-        .status(400)
-        .json({
-          message: "Price must be greater than 0 and stock cannot be negative",
-        });
+      return res.status(400).json({
+        message: "Price must be greater than 0 and stock cannot be negative",
+      });
     }
 
     const images = req.files?.map((file) => file.path) || [];
@@ -59,13 +56,27 @@ export async function createProduct(req, res) {
     return res.status(500).json({ message: "Internal server error" });
   }
 }
-// FUNCTION TO GET ALL PRODUCTS 
-export async function getAllProducts(req,res) {
+// FUNCTION TO GET ALL PRODUCTS
+export async function getAllProducts(req, res) {
+  try {
+    const allProduct = await productModel
+      .find()
+      .populate("ownerId", "userName email")
+      .populate("shopId", "shopName address");
 
-    try {
-        
-    } catch (error) {
-        
-    }
-    
+    return res.status(200).json(allProduct);
+  } catch (error) {
+    console.log("error in get all product function", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+// FUNCTION TO GET SPECIFIC PERSON/OWNER PRODUCTS
+export async function getOwnerProducts(req, res) {
+  try {
+    const products = await productModel.find({ ownerId: req.user._id });
+    return res.status(200).json(products);
+  } catch (error) {
+    console.log("error in get specific owner product function", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 }
--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 import uploadProduct from '../middleware/uploadProductImages.js';
-import { createProduct } from '../controller/productController.js';
+import { createProduct, getAllProducts, getOwnerProducts } from '../controller/productController.js';
 
 const productRouter = express.Router();
 
 productRouter.post('/create',uploadProduct.array('images',4), authMiddleware,createProduct);
-
+productRouter.get('/allproducts',getAllProducts);
+productRouter.get('/ownerproducts',authMiddleware,getOwnerProducts);
 
 export default productRouter
