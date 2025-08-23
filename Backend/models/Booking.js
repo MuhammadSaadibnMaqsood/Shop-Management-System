4292c9a54926a07b92e429bf4a5385b6b74332e8
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-08-23 10:11:47 -0700

    Work on booking schema

---
+++ Backend/models/Booking.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    shop: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Shop",
+        required: true,
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+    },
+    warrantyExpiry: { 
+        type: Date,
+    },
+    qrCode: { 
+        type: String,
+    }
+}, {timestamps: true});
+
+const bookingModel = mongoose.model("Booking", bookingSchema);
+export default bookingModel;
