6e5efd65b291a3d1362fb330bfe2add536be9200
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-03 05:23:25 -0700

    Working on home page testemonial section

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,22 +1,21 @@
 import Hero from "../components/Hero";
 import Products from "../components/Products";
+import Testimonail from "../components/Testimonail";
 
 const Home = () => {
   return (
     <>
-    <div>
-      <div className=" w-[100%] sticky top-0">
-        
-      <Hero />
-      </div>
-      <div className="h-[100%] w-[100%] sticky top-0 overflow-hidden">
-        
-      <Products />
-      </div>
+      <div>
+        <div className=" w-[100%] sticky top-0">
+          <Hero />
+        </div>
+        <div className="h-[100%] w-[100%] sticky top-0 overflow-hidden">
+          <Products />
+        </div>
 
-    </div>
-      <div className="w-full h-screen bg-black">
-
+        <div className=" h-screen w-full sticky top-0 overflow-hidden bg-white">
+          <Testimonail />
+        </div>
       </div>
     </>
   );
