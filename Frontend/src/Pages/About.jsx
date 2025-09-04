57337b05a714f46cb75e87458d542b331e852d6b
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-04 05:31:38 -0700

    Working on about

--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import { delay, motion } from "framer-motion";
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="h-screen w-full relative bg-black text-white flex items-center justify-center">
+        <motion.img
+        
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 2 }}
+          className="h-full"
+          src="/shoe4.webp"
+          alt=""
+        />
 
-export default About
+        <motion.h1
+        initial={{opacity:0,x:-100}}
+        animate={{opacity:1,x:0}}
+        transition={{delay:0.5,duration:1.5}}
+        className="text-7xl absolute top-35 right-100 AsimovianFont">AURA</motion.h1>
+      </div>
+    </div>
+  );
+};
+
+export default About;
