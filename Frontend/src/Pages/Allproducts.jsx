54f881caa7f6da87791977d8203d274f1869bc77
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-05 09:43:27 -0700

    Complete all product page heading now workking on showing cards

--- Frontend/src/Pages/Allproducts.jsx
+++ Frontend/src/Pages/Allproducts.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import ProductCard from "../components/ProductCard";
+const dummyProducts = [
+  {
+    _id: "1",
+    productName: "Nike Air Zoom",
+    price: 120,
+    category: "shoes",
+    images: ["/uploads/shoe1.jpg", "/uploads/shoe2.jpg"],
+  },
+  {
+    _id: "2",
+    productName: "Adidas Ultraboost",
+    price: 150,
+    category: "shoes",
+    images: ["/uploads/shoe3.jpg"],
+  },
+  {
+    _id: "3",
+    productName: "Casual Cotton Shirt",
+    price: 40,
+    category: "shirts",
+    brand: "Levis",
+    images: ["/uploads/shirt1.jpg"],
+  },
+];
 
 const Allproducts = () => {
   return (
-    <div>Allproducts</div>
-  )
-}
+    <div className="bg-zinc-950">
+      <motion.h1
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.5, ease: "easeOut" }}
+        viewport={{ once: true }}
+        className="AsimovianFont pt-10 bg-gradient-to-r from-[#c6005c] to-[#4a00b8] bg-clip-text text-transparent text-center text-6xl md:text-7xl font-bold"
+      >
+        Product we have !
+      </motion.h1>
 
-export default Allproducts
+      <div>
+        {dummyProducts.map((item) => {
+          <ProductCard
+          key={item._id}
+            productName={item.productName}
+            price={item.price}
+            image={item.images[0]}
+            category = {item.category}
+          />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Allproducts;
