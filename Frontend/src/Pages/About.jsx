ae1cade3f97cc086de7766dc05079b7f39cd5718
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-05 09:20:49 -0700

    Complete about page

--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const About = () => {
   return (
     <div>
-      <div className="h-screen w-full relative bg-black text-white flex flex-col xl:flex-row items-center justify-center">
+      <div className="h-screen w-full relative bg-black text-white flex flex-col xl:flex-row  items-center p-12 xl:p-0 justify-start xl:justify-center">
         {/* Image */}
         <motion.img
           initial={{ opacity: 0, y: 100 }}
@@ -24,14 +24,14 @@ const About = () => {
           initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ delay: 0.5, duration: 1 }}
-          className="text-center group hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r from-[#c6005c] to-[#4a00b8] transition-all duration-700 ease-in-out md:text-9xl text-5xl sm:text-6xl static xl:absolute xl:top-35 xl:right-55 AsimovianFont drop-shadow-[0_5px_15px_rgba(200,0,200,0.3)]"
+          className="text-center group hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r from-[#c6005c] to-[#4a00b8] transition-all duration-700 ease-in-out md:text-9xl text-9xl static xl:absolute xl:top-35 xl:right-55 AsimovianFont drop-shadow-[0_5px_15px_rgba(200,0,200,0.3)]"
         >
           AURA
           <motion.span
             initial={{ opacity: 0 }}
             animate={{ opacity: 0.3 }}
             transition={{ duration: 2 }}
-            className="group-hover:bg-clip-text group-hover:text-transparent group-hover:bg-gradient-to-r from-[#c6005c] to-[#4a00b8] transition-all duration-700 ease-in-out absolute AsimovianFont left-0 right-0 top-full text-5xl sm:text-6xl md:text-9xl font-bold tracking-widest text-gray-400 transform scale-y-[-1] blur-sm select-none"
+            className="group-hover:bg-clip-text group-hover:text-transparent group-hover:bg-gradient-to-r from-[#c6005c] to-[#4a00b8] transition-all duration-700 ease-in-out absolute AsimovianFont left-0 right-0 top-full text-9xl  md:text-9xl font-bold tracking-widest text-gray-400 transform scale-y-[-1] blur-sm select-none"
           >
             AURA
           </motion.span>
@@ -42,7 +42,7 @@ const About = () => {
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 2.5 }}
-          className="static xl:absolute top-80 left-60 w-96 text-gray-300 leading-relaxed"
+          className="static pt-20 xl:p-0 xl:absolute top-80 left-60 w-full text-center xl:w-96 text-gray-300 leading-relaxed"
         >
           This role-based e-commerce platform allows customers to explore and
           purchase products effortlessly, while shop owners and admins can
