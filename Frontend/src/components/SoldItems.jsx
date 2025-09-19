359c17383f40d05e52536369d8c05f08a386f4ba
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2025-09-19 07:40:20 -0700

    UI Complete of sold item now its time to make an API that change payment status PENDING to DONE

--- Frontend/src/components/SoldItems.jsx
+++ Frontend/src/components/SoldItems.jsx
@@ -22,9 +22,9 @@ const SoldItems = () => {
                 <th className="px-6 py-3 hidden sm:block  border-b border-zinc-800">
                   Payment
                 </th>
-                <th className="px-6 py-3 border-b border-zinc-800">Stock</th>
+                <th className="px-6 py-3 border-b border-zinc-800">Address</th>
                 <th className="px-6 py-3 border-b hidden md:block border-zinc-800">
-                  Sell
+                  Delivered
                 </th>
               </tr>
             </thead>
@@ -39,7 +39,7 @@ const SoldItems = () => {
                   </td>
                   <td
                     className={`${
-                      product.paymentStatus
+                      product.paymentStatus == "PENDING"
                         ? "text-green-500"
                         : "text-orange-400"
                     }px-6 py-4 hidden sm:block border-b border-zinc-800 `}
@@ -47,10 +47,28 @@ const SoldItems = () => {
                     {product.paymentStatus}
                   </td>
                   <td className="px-6 py-4 border-b border-zinc-800">
-                    {product.product.stock || 0}
+                    {product.address || 0}
                   </td>
                   <td className="px-6 py-4 hidden md:block border-b border-zinc-800 text-blue-400">
-                    {product.product.sell || 0}
+                    <label className="relative flex items-center cursor-pointer">
+                      <input type="checkbox" className="peer hidden" />
+                      <span className="w-5 h-5 border-2 border-blue-400 rounded-md peer-checked:bg-blue-500 peer-checked:border-blue-500 transition-all duration-300 flex items-center justify-center">
+                        <svg
+                          className="w-3 h-3 text-white opacity-0 peer-checked:opacity-100 transition-opacity duration-200"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="3"
+                            d="M5 13l4 4L19 7"
+                          />
+                        </svg>
+                      </span>
+                    </label>
                   </td>
                 </tr>
               ))}
